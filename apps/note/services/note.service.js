2c937b59082d7cc86023b0cce03713adec652d5b
Aviram Bach <bach2142@gmail.com>
2023-09-06 16:08:37 +0300

    aviram upload

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,1 +1,135 @@
-// note service
+import { storageService } from '../../../services/async-storage.service.js'
+import { utilService } from '../services/util.service.js'
+
+
+const NOTES_KEY = 'notesDB'
+// var gFilterBy = { title: '', price: 0, publishedDate: 0 }
+let notes = [{
+    id: 'n101',
+    createdAt: 1112222,
+    type: 'NoteTxt', isPinned: true,
+    style: {
+        backgroundColor: '#00d'
+    },
+    info: {
+        txt: 'Fullstack Me Baby!'
+    }
+},
+{
+    id: 'n102',
+    type: 'NoteImg',
+    isPinned: false,
+    info: {
+        url: 'http://some-img/me',
+        title: 'Bobi and Me'
+    },
+    style: {
+        backgroundColor: '#00d'
+    }
+},
+{
+    id: 'n103',
+    type: 'NoteTodos',
+    isPinned: false,
+    info:
+    {
+        title: 'Get my stuff together',
+        todos: [
+            { txt: 'Driving license', doneAt: null },
+            { txt: 'Coding power', doneAt: 187111111 }
+        ]
+    }
+}
+]
+
+_createNotes()
+
+
+export const noteService = {
+    query,
+    get,
+    remove,
+    save,
+    // getEmptyBook,
+    // getEmptyReview,
+    // getNextBookId,
+    // getFilterBy,
+    // setFilterBy,
+    // getDefaultFilter,
+    // addReview,
+    // deleteReview,
+}
+
+
+
+function query(filterBy) {
+    return storageService.query(NOTES_KEY).then(notes => {
+        //   if (filterBy.title) {
+        //     const regex = new RegExp(filterBy.title, 'i')
+        //     books = books.filter(book => regex.test(book.title))
+        //   }
+        //   if (filterBy.price) {
+        //     books = books.filter(book => book.listPrice.amount >= filterBy.price)
+        //   }
+        //   if (filterBy.publishedDate) {
+        //     books = books.filter(book => book.publishedDate >= filterBy.publishedDate)
+        //   }
+        return notes
+    })
+}
+
+function get(noteId) {
+    return storageService.get(NOTES_KEY, noteId)
+}
+
+function remove(noteId) {
+    return storageService.remove(NOTES_KEY, noteId)
+}
+
+function save(note) {
+    if (note.id) {
+      return storageService.put(NOTES_KEY, note)
+    } else {
+      return storageService.post(NOTES_KEY, note)
+    }
+}
+
+// function getEmptyBook() {
+//     return {
+//       title: '',
+//       subtitle: '',
+//       authors: [],
+//       publishedDate: 1900,
+//       description: '',
+//       pageCount: 0,
+//       categories: [],
+//       thumbnail: '../assets/imgs/20.jpg',
+//       language: 'en',
+//       listPrice: {
+//         amount: 0,
+//         currencyCode: 'EUR',
+//         isOnSale: false,
+//       },
+//     }
+//   }
+
+function _getAllNotes(){
+    return notes
+}
+
+function _createNotes() {
+    let notes = loadFromStorage(NOTES_KEY)
+    if (!notes || !notes.length) {
+        notes = _getAllNotes()
+        saveToStorage(NOTES_KEY, notes)
+    }
+}
+
+function saveToStorage(key, val) {
+    localStorage.setItem(key, JSON.stringify(val))
+}
+
+function loadFromStorage(key) {
+    var val = localStorage.getItem(key)
+    return JSON.parse(val)
+}
--- apps/note/views/NoteIndex.jsx
+++ apps/note/views/NoteIndex.jsx
@@ -1,4 +1,38 @@
+import { NoteList } from "../cmps/NoteList.jsx"
+import { noteService } from '../services/note.service.js'
+
+const { useState, useEffect } = React
+const { Link } = ReactRouterDOM
+
 
 export function NoteIndex() {
-    return <div>note app</div>
+
+    const [notes, setNotes] = useState(null)
+
+    
+    useEffect(() => {
+        noteService.query().then(notes => {
+            setNotes(notes)
+        })
+    }, [])
+    
+    function onRemoveNote(noteId) {
+        noteService
+          .remove(noteId)
+          .then(() => {
+            setNotes(prevNotes => prevNotes.filter(note => note.id !== noteId))
+            // showSuccessMsg(`Note Removed! ${noteId}`)
+          })
+          .catch(err => {
+            console.error(err)
+            // showErrorMsg(`Problem Removing ${noteId}`)
+          })
+      }
+
+    console.log(notes)
+    
+    if (!notes) return <div>Loading notes...</div>
+    return <div>
+         <NoteList notes={notes} onRemoveNote={onRemoveNote} />
+    </div>
 }
